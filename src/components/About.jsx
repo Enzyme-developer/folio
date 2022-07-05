14498d923166576b29948cd9693bfce1008ee10d
Ayoe <anomalous189@gmail.com>
2022-07-05 13:25:33 +0100

    about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,15 +5,15 @@ const About = () => {
     <div className='mt-16 p-4 w-full flex-col lg:flex-row lg:flex-row-reverse flex items-center justify-center lg:justify-between lg:gap-16'>
         <p className='text-gray-400 text-2xl lg:w-[70vw]'>
           I'm a <span className='text-gray-100'>passionate Frontend Engineer</span>  proficient in various frontend technologies. I am focused on creating 
-          <span className='text-gray-100'>seamless experiences and interactions</span> on the web 
+          <span className='text-gray-100'> seamless experiences and interactions</span> on the web 
           by tranforming designs and ideas, <span className='text-gray-100'>pixel perfectly</span>, to code.
           I translate needs into functional and appealing interactive applications.
           I work with <span className='text-gray-100'>Javascript, Typescript, React Js and Next Js</span> and much more on the client side.
           I have an understanding of the web development process (design, development and deployment), 
           understanding of layout aesthetics coupled with Knowledge of SEO principles.
           I Gather and refine specifications and requirements based on technical requirements or UX design .
-          I'm a good <span className='text-gray-100'>team player</span> an also a fast learner.
-          I recently started exploring <span className='text-gray-100'>blockchain development</span>, otherwise known as WEB3.
+          I'm a <span className='text-gray-100'> good team player & a fast learner</span>.
+          I recently started exploring <span className='text-gray-100'>blockchain development</span>, otherwise known as <span>WEB3.</span>
           I also write <span className='text-gray-100'>technical articles</span> so as to clearly and effectively explain technical topics and to reinforce my knowledge.
           When I'm not <span className='text-gray-100'>striking a balance</span> between functionality and aesthetic design or writing technical articles, 
           you can find me catching up on my favorite movies.
