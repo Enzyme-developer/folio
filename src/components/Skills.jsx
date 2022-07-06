9b9e84b537ad30beb455767b93f3775f54781aa0
Ayoe <anomalous189@gmail.com>
2022-07-06 08:50:07 +0100

    skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,83 +4,83 @@ const Skills = () => {
     return (
     <div className='p-4 w-full'>
         <h1 className='mt-12 text-white text-4xl mb-10'>My Tech Stack</h1>
-        <div className= 'grid grid-cols-3 md:grid-cols-6 w-full gap-y-10'>
+        <div className= 'grid grid-cols-3 md:grid-cols-6 w-full gap-10'>
           
-        <div className='flex flex-col justify-center gap-y-6'>
+        <div className='flex flex-col justify-center gap-y-10'>
         <img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/javascript-colored.svg" width="36" height="36" alt="JavaScript" />
         <p className='text-gray-100 uppercase'>Javascript</p>
         </div>
           
-        <div className='flex flex-col justify-center gap-y-6'>
+        <div className='flex flex-col justify-center gap-y-10'>
         <img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/typescript-colored.svg" width="36" height="36" alt="TypeScript" />
         <p className='text-gray-100 uppercase'>Typescript</p>
         </div>
 
-        <div className='flex flex-col justify-center gap-y-6'>
+        <div className='flex flex-col justify-center gap-y-10'>
         <img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/tailwindcss-colored.svg" width="36" height="36" alt="TailwindCSS" />
         <p className='text-gray-100 uppercase'>tailwind css</p>
         </div>
 
-        <div className='flex flex-col justify-center gap-y-6'>
+        <div className='flex flex-col justify-center gap-y-10'>
         <img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/html5-colored.svg" width="36" height="36" alt="HTML5" />
         <p className='text-gray-100 uppercase'>Html5</p>
         </div>
 
-        <div className='flex flex-col justify-center gap-y-6'>
+        <div className='flex flex-col justify-center gap-y-10'>
         <img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/react-colored.svg" width="36" height="36" alt="React" />
         <p className='text-gray-100 uppercase'>React</p>
         </div>
 
-        <div className='flex flex-col justify-center gap-y-6'>
+        <div className='flex flex-col justify-center gap-y-10'>
         <img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/nextjs-colored-dark.svg" width="36" height="36" alt="NextJs" />
         <p className='text-gray-100 uppercase'>Next js</p>
         </div>
 
-        <div className='flex flex-col justify-center gap-y-6'>
+        <div className='flex flex-col justify-center gap-y-10'>
         <img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/redux-colored.svg" width="36" height="36" alt="Redux" />
         <p className='text-gray-100 uppercase'>Redux</p>
         </div>
 
-        <div className='flex flex-col justify-center gap-y-6'>
+        <div className='flex flex-col justify-center gap-y-10'>
         <img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/firebase-colored.svg" width="36" height="36" alt="Firebase" />
         <p className='text-gray-100 uppercase'>Firebase</p>
         </div>
 
-        <div className='flex flex-col justify-center gap-y-6'>
+        <div className='flex flex-col justify-center gap-y-10'>
         <img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/nodejs-colored.svg" width="36" height="36" alt="NodeJS" />
         <p className='text-gray-100 uppercase'>Node js</p>
         </div>
 
-        <div className='flex flex-col justify-center gap-y-6'>
+        <div className='flex flex-col justify-center gap-y-10'>
         <img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/sass-colored.svg" width="36" height="36" alt="Sass" />
         <p className='text-gray-100 uppercase'>Sass</p>
         </div>
 
-        <div className='flex flex-col justify-center gap-y-6'>
+        <div className='flex flex-col justify-center gap-y-10'>
         <img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/css3-colored.svg" width="36" height="36" alt="CSS3" />
         <p className='text-gray-100 uppercase'>Css3 [BEM]</p>
         </div>
 
-        <div className='flex flex-col justify-center gap-y-6'>
+        <div className='flex flex-col justify-center gap-y-10'>
         <img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/materialui-colored.svg" width="36" height="36" alt="Material UI" />
         <p className='text-gray-100 uppercase'>Material UI</p>
         </div>
         
 
-        <div className='flex flex-col justify-center gap-y-6'>
+        <div className='flex flex-col justify-center gap-y-10'>
         <img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/bootstrap-colored.svg" width="36" height="36" alt="Bootstrap" />
         <p className='text-gray-100 uppercase'>Bootstrap</p>
         </div>
 
 
-        <div className='flex flex-col justify-center gap-y-6'>
+        <div className='flex flex-col justify-center gap-y-10'>
         <img src="https://e7.pngegg.com/pngimages/713/558/png-clipart-computer-icons-pro-git-github-logo-text-logo-thumbnail.png" width="36" height="36" alt="git" />
         <p className='text-gray-100 uppercase'>Git</p>
         </div>
 
-        <div className='flex flex-col justify-center gap-y-6'>
+        <div className='flex flex-col justify-center gap-y-10'>
         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCsASCy83izYeNHlYSp4stqfcCz4g1wg78KbAA_hrnOb72sKQMn0B5eLctnwybLDBphNc&usqp=CAU" width="36" height="36" alt='github' />
-        <p className='text-gray-100 uppercase'>Git</p>
+        <p className='text-gray-100 uppercase'>Github</p>
         </div>
 
     </div>
