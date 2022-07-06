c0322140b004a8d8dd39b95e29a7d35824effba8
Ayoe <anomalous189@gmail.com>
2022-07-06 08:57:18 +0100

    skills

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import './App.css';
-import { BrowserRouter , Routes , Route } from 'react-router-dom'
+// import { BrowserRouter , Routes , Route } from 'react-router-dom'
 import Hero from './components/Hero';
 import Navbar from './components/Navbar'
 import About from './components/About';
 import Projects from './components/Projects';
 import Articles from './components/Articles';
 import Skills from './components/Skills';
+import Contact from './components/Contact';
 
 function App() {
   return (
     <div className="bg-black min-h-screen overflow-hidden">
-      <BrowserRouter>
-        <Navbar />
-        <Hero />
-        <About />
-        <Skills />
-        <Projects />
-        <Articles />
-        <Routes>
-          {/* <Route path='/' element={<About />} /> */}
-        </Routes>
-    </BrowserRouter>
+      <Navbar />
+      <Hero />
+      <About />
+      <Skills />
+      <Projects />
+      <Articles />
+      <Contact />
     </div>
   );
 }
