a99ecf9234a94dca97cca50110fddab85d0c8b66
Ayoe <anomalous189@gmail.com>
2022-07-06 20:02:51 +0100

    footer

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,11 +2,8 @@ import React from 'react'
 
 const Skills = () => {
     return (
-    <div className='p-4 w-full my-16'>
-        <h1 className='mt-12 text-white text-4xl uppercase my-8'>Technical Skills</h1>
-
     <div className='p-4 w-full my-8'>
-        <h1 className='mt-12 text-white text-4xl my-8'>My Tech Stack</h1>
+        <h1 className='mt-12 text-white text-4xl mb-8'>Technical skills</h1>
 
         <div className= 'grid grid-cols-3 md:grid-cols-6 w-full gap-8'>
 
@@ -122,7 +119,6 @@ const Skills = () => {
         </div>
 
       </div>
-    </div>
     </div>
        
   )
