d11d83ca78f4b3f36c4108334cc8cf355c4efa28
Ayoe <anomalous189@gmail.com>
2022-07-07 14:46:07 +0100

    footer

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const About = () => {
   return (
     <div id='about' className='overflow-hidden mt-16 p-4 w-full flex-col lg:flex-row-reverse flex items-center justify-center lg:justify-between lg:gap-16'>
-        <p className='text-gray-500 text-2xl lg:w-[70vw]'>
+      <div>
+        <p className='text-gray-400 text-2xl lg:w-[70vw]'>
           I'm a <span className='text-gray-100'>passionate Frontend Engineer</span>  proficient in various frontend technologies. I am focused on creating 
           <span className='text-gray-100'> seamless experiences and interactions</span> on the web 
           by tranforming designs and ideas, <span className='text-gray-100'>pixel perfectly</span>, to code.
@@ -18,9 +19,14 @@ const About = () => {
           I also write <span className='text-gray-100'>technical articles</span> so as to clearly and effectively explain technical topics and to reinforce my knowledge.
           When I'm not <span className='text-gray-100'>striking a balance</span> between functionality and aesthetic design or writing technical articles, 
           you can find me catching up on my favorite movies.
-        </p>
-
+      </p>
+       <p className='mt-10 rounded-lg'><a className='bg-blue-500 text-white p-4 mt-10 rounded textxll font-bold' href='https://docs.google.com/document/d/11WARLeIufnd2PR_70pSIxPQ_2haXuU2MY6XEUBKApDQ/'>My Resume</a></p>
+    </div>
+      
+      <div>
         <img src='https://ayoo-dev.netlify.app/avatar.jpg' alt='avatar' className='rounded-lg w-full md:w-[400px] my-16 '/>
+      </div>
+      
     </div>
   )
 }
