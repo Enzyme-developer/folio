0020ae370222742d4e5bddd4a3bf09e651c33dcc
Ayoe <anomalous189@gmail.com>
2022-07-12 14:09:20 +0100

    first commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -72,7 +72,9 @@ const About = () => {
           you can find me catching up on my favorite movies.
       </p>
    
-       <p ref={addToRefs} className='mt-10 rounded-lg inline-block'><a className='bg-blue-500 text-white p-4 mt-10 rounded text-xl font-bold' href='https://docs.google.com/document/d/11WARLeIufnd2PR_70pSIxPQ_2haXuU2MY6XEUBKApDQ/edit?usp=sharing'>My Resume</a></p>
+       <p ref={addToRefs} className='mt-10 rounded-lg inline-block'>
+          <a className='bg-blue-500 text-white p-4 mt-10 rounded text-xl font-bold' href='https://drive.google.com/file/d/1Yq4Ede1dBpif4_u3hStkVFHRulKkKc4g/view?usp=sharing'>My Resume</a>
+       </p>
        <p ref={addToRefs} className='mt-10 rounded-lg  inline-block ml-10'><a className='bg-blue-500 text-white p-4 mt-10 rounded text-xl font-bold' href='https://www.linkedin.com/in/ayomide-talabi/'>LinkedIn</a></p>
   
        </div>
